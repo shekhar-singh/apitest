7bf192ef292b1fd2c58a27fe0af446943cb0bef2
shekhar-singh <sschoudhary60@gmail.com>
2015-11-10 02:31:42 +0530

    api url

---
+++ demoapi/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return HttpResponse("Hello,This is my first api :-)")
+
+
+def index(request):
+	return HttpResponse("<h2>Type api in url to test available apis </h2>")
